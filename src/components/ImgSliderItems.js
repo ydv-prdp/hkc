9bb1abd278693358d1e253ebb27aefbe3b3396ef
Pradeep Yadav <pradeepyadav101010@gmail.com>
2023-06-21 15:10:22 +0530

    added website of hkc

---
+++ src/components/ImgSliderItems.js
@@ -0,0 +1,53 @@
+export const ImgSliderItems = [
+    {
+        id:1,
+        title:"Home",
+        imgArr:[
+            {
+                id:1,
+                imgSrc:"./images/imgslider1.jpg"
+            },
+            {
+                id:2,
+                imgSrc:"./images/imgslider2.jpg"
+            },
+            {
+                id:3,
+                imgSrc:"./images/imgslider3.jpg"
+            },
+            {
+                id:4,
+                imgSrc:"./images/imgslider4.jpg"
+            },
+            {
+                id:5,
+                imgSrc:"./images/imgslider5.jpg"
+            },
+            {
+                id:6,
+                imgSrc:"./images/imgslider6.jpg"
+            },
+
+        ]
+    },
+   
+]
+
+export const ImgSliderACat = [
+    {
+        id:2,
+        title:"ACat",
+        imgArr:[
+            {
+                id:1,
+                imgSrc:"./logo192.png"
+            },
+            {
+                id:2,
+                imgSrc:"./logo512.png"
+            },
+
+        ]
+    },
+   
+]
---
+++ src/helper/NoticeList.js
@@ -0,0 +1,36 @@
+export const NoticeList = [
+   
+            {
+                id:1,
+                title:"Book List 2023-24",
+                notice:"1.pdf"
+            },
+            {
+                id:2,
+                title:"Pre Board Date Sheet X 2022-23 ",
+                notice:"2.pdf"
+            },
+            {
+                id:3,
+                title:"Pre Board Date Sheet XII 2022-23 ",
+                notice:"3.pdf"
+            },
+            {
+                id:4,
+                title:"School Calender ",
+                notice:"3.pdf"
+            },
+            {
+                id:5,
+                title:"Gallery ",
+                notice:"3.pdf"
+            },
+            {
+                id:6,
+                title:"Co-Currlicular Activities ",
+                notice:"3.pdf"
+            },
+
+        ]
+    
+
